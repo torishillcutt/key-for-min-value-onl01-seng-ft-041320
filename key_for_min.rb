149643b312963f42424a4515a180e7d0e9400216
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-17 03:59:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,12 +1,18 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
-   name_hash.any? do |key, value|
-     if value == 1 || value == 10
-       return key
-     elsif value.nil?
-       return nil
-    end
-  end
+ name_hash.reduce{ |f, s| f.last > s.last ? s : f }.first
 end
+
+
+
+
+#def key_for_min_value(name_hash)
+#   name_hash.any? do |key, value|
+#     if value == 1 || value == 10
+#       return key
+#     elsif value.nil?
+#       return nil
+#    end
+#  end
+#end
