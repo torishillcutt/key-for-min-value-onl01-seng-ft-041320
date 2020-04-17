965637a4c90ed7b86fb596824806fcb8b5d201d9
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-17 03:47:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   dif_hash = {}
    name_hash.any? do |key, value|
      if value == 1 || value == 10
-       diff_hash << key
+       diff_hash << "#{key}"
     end
   end
   diff_hash
