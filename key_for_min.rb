db5cbdf37406f44cb6f85feffa0e653ad97d320d
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-17 03:49:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  dif_hash = {}
    name_hash.any? do |key, value|
      if value == 1 || value == 10
-       diff_hash << key
+       return key
     end
   end
-  diff_hash
 end
