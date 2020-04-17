b1a84fc7ed3f2cc648317f893e5093899d5560c9
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-17 03:41:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,9 @@
 
 def key_for_min_value(name_hash)
    name_hash.any? do |key, value|
+     if value = {} return nil
      if value == 1 || value == 10
        return key
-     else
-       nil
     end
   end
 end
