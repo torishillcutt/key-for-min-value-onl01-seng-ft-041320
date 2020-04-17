7e5a17b3673bb9ae655c7648a7cf5381a61e102d
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-17 03:16:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-   name_hash.sort
-   name_hash.pop
+   name_hash.collect do |food, amount|
 end
